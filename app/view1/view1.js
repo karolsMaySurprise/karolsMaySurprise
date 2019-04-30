d6325678f79e583a8babe7e4bd76fa2da74ca972
karolsMaySurprise <nardo.are@outlook.com>
2019-04-30 03:48:43 -0700

    DEV-1 Adding logic and base64 encoded url

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,73 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+    function decode64(input) {
+        var keyStr = "ABCDEFGHIJKLMNOP" +
+            "QRSTUVWXYZabcdef" +
+            "ghijklmnopqrstuv" +
+            "wxyz0123456789+/" +
+            "=";
+        var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
+
+        // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+        var base64test = /[^A-Za-z0-9\+\/\=]/g;
+        if (base64test.exec(input)) {
+            alert("There were invalid base64 characters in the input text.\n" +
+                "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                "Expect errors in decoding.");
+        }
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        do {
+            enc1 = keyStr.indexOf(input.charAt(i++));
+            enc2 = keyStr.indexOf(input.charAt(i++));
+            enc3 = keyStr.indexOf(input.charAt(i++));
+            enc4 = keyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+
+            chr1 = chr2 = chr3 = "";
+            enc1 = enc2 = enc3 = enc4 = "";
+
+        } while (i < input.length);
+
+        return unescape(output);
+    }
+
+  var url = "aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9odzJrYW4xdGJlbnRjZWEvNTM5MzE3ODI1LnBkZj9kbD0w"
+
+  $scope.currentDate = new Date()
+  $scope.itsTime = $scope.currentDate.getMonth() + 1 == 5 && $scope.currentDate.getFullYear() == 2019 && $scope.currentDate.getDate() == 6
+
+  if($scope.itsTime){
+      $scope.linkDescription = "YEW ITS TIME";
+      $scope.linkDescription = "CLICK ME";
+      $scope.linkUrl = decode64(url);
+  }
+  else{
+    $scope.linkDescription = "Sorry not yet :(";
+    $scope.linkUrl = "";
+  }
+
+    setInterval(function() {
+        $scope.currentDate = new Date()
+    }, 1000);
+
 
 }]);
